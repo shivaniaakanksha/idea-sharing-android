c863210a3a1a7adc5cef5bdc128df5ab5e3612c2
Aakash Goel <goelaakash79@gmail.com>
2019-04-15 23:53:53 +0530

    terms of use added

--- app/src/main/java/tech/dsckiet/ideasharing/AuthActivity.java
+++ app/src/main/java/tech/dsckiet/ideasharing/AuthActivity.java
@@ -1,11 +1,14 @@
 package tech.dsckiet.ideasharing;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -30,7 +33,7 @@ public class AuthActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 1001;
     private static final String TAG = AuthActivity.class.getSimpleName();
 
-//    TextView termsOfUseBtn, privacyPolicyBtn;
+    TextView termsOfUseBtn;
     CardView mGoogleSignInButton;
 
     private FirebaseAuth mAuth;
@@ -47,22 +50,22 @@ public class AuthActivity extends AppCompatActivity {
 
         layout = findViewById(R.id.layout);
 
-//        termsOfUseBtn = findViewById(R.id.termsofusebtn);
-//
-//        termsOfUseBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                AlertDialog.Builder alert = new AlertDialog.Builder(AuthActivity.this);
-//                alert.setTitle("Terms of use");
-//                alert.setMessage("Every landing page needs a small description after the big bold title, that's why we added this text here. Add here all the information that can make you or your product create the first impression.");
-//                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-//                    public void onClick(DialogInterface dialog, int whichButton) {
-//                    }
-//                });
-//                alert.show();
-//
-//            }
-//        });
+        termsOfUseBtn = findViewById(R.id.termsofuse);
+
+        termsOfUseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(AuthActivity.this);
+                alert.setTitle("Terms of use");
+                alert.setMessage("We request you to turn on your messaging services from your payment appliations, in order to use our application.");
+                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                    }
+                });
+                alert.show();
+
+            }
+        });
 
 
 //        mSignOutButton = findViewById(R.id.google_sign_out);
